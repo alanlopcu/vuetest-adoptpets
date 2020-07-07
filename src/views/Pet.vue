3a4c111dda75fea52b95b11138f4a302b36f41fe
Alvaro Lopez <alanlopcu@gmail.com>
2020-07-07 19:50:39 +0200

    animals pages, routes and show their data

---
+++ src/views/Pet.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <!-- {{ $route.params.species }}
+    {{ $route.params.id }} -->
+    <h1>{{ this.animal.name }} ({{ $route.params.species }})</h1>
+    <p>Age: {{ animal.age }} years old</p>
+    <p>Breed: {{ animal.breed }}</p>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data () {
+    return {
+      animal: {}
+    }
+  },
+  computed: {
+    // cats() {
+    //   return this.$store.state.cats
+    // }
+    ...mapState([
+      'cats',
+      'dogs'
+    ])
+  },
+  mounted () {
+    const animal = this[this.$route.params.species][this.$route.params.id]
+    this.animal = animal
+  }
+  // methods: {
+  //   pet () {
+  //     const animal = this[this.$route.params.species][this.$route.params.id]
+  //     return animal
+  //   }
+  // }
+}
+</script>
